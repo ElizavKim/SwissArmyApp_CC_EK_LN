ad1562ca8cbccb0f78f1684cb9518532468718b7
ElizavKim <elizav.kim@gmail.com>
2023-04-27 20:46:14 -0400

    Update Settings.java

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/Settings.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/Settings.java
@@ -4,10 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,10 +14,10 @@ import java.io.IOException;
 public class Settings {
     @FXML
     private ToggleButton colorToggle;
-    @FXML
-    private boolean redMode = true;
-    @FXML
-    private AnchorPane anchorPane;
+//    @FXML
+//    private boolean redMode = true;
+//    @FXML
+//    private AnchorPane anchorPane;
 
 
     @FXML
